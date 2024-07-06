099868c82c343c63b3cc06c47a7b4501d68cf942
Manthan997 <114974274+Manthan997@users.noreply.github.com>
2024-07-06 13:34:00 +0000

    message is entered

---
+++ rusty/target/debug/deps/rusty-14f4af4a9b13e8b9.d
@@ -0,0 +1,5 @@
+/workspaces/Fe2O3/rusty/target/debug/deps/rusty-14f4af4a9b13e8b9: src/main.rs
+
+/workspaces/Fe2O3/rusty/target/debug/deps/rusty-14f4af4a9b13e8b9.d: src/main.rs
+
+src/main.rs:
---
+++ rusty/target/debug/rusty.d
@@ -0,0 +1,1 @@
+/workspaces/Fe2O3/rusty/target/debug/rusty: /workspaces/Fe2O3/rusty/src/main.rs
