b011326f18ef3d601b27888d25a4deb3f0994287
Manthan997 <114974274+Manthan997@users.noreply.github.com>
2024-07-06 13:43:36 +0000

    docker and codespaces

---
+++ rusty/target/debug/deps/rusty-8a9ef958cf333d64.d
@@ -0,0 +1,5 @@
+/workspaces/Fe2O3/rusty/target/debug/deps/librusty-8a9ef958cf333d64.rmeta: src/main.rs
+
+/workspaces/Fe2O3/rusty/target/debug/deps/rusty-8a9ef958cf333d64.d: src/main.rs
+
+src/main.rs:
---
+++ rusty/target/debug/deps/rusty-f1ede73e68d63145.d
@@ -0,0 +1,5 @@
+/workspaces/Fe2O3/rusty/target/debug/deps/librusty-f1ede73e68d63145.rmeta: src/main.rs
+
+/workspaces/Fe2O3/rusty/target/debug/deps/rusty-f1ede73e68d63145.d: src/main.rs
+
+src/main.rs:
